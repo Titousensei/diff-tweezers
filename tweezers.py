dc58adf7caa96faddca6dc117c63a8f2430c6259
Eric Gaudet <gaudet.eric@gmail.com>
2024-03-18 14:33:23 -0700

    indent 4

--- tweezers.py
+++ tweezers.py
@@ -6,8 +6,8 @@ import sys
 
 # Diff struct:
 # - file ("diff", "---", "+++")
-#   - chunk "@@"
-#     - lines
+#     - chunk "@@"
+#         - lines
 
 PREFIX_FOLDED = '>'
 PREFIX_CHUNK = "@@ "
@@ -16,189 +16,208 @@ FOLDED_CHUNK = PREFIX_FOLDED + PREFIX_CHUNK
 FOLDED_DIFF = PREFIX_FOLDED + PREFIX_DIFF
 
 class FoldingDiff:
-  def __init__(self, lines, files, chunks):
-    self.lines = lines
-    self.files = files
-    self.chunks = chunks
-    self._folded = set()
-    self._selected = set()
-    self.offset = 0
+    def __init__(self, lines, files, chunks):
+        self.lines = lines
+        self.files = files
+        self.chunks = chunks
+        self._folded = set()
+        self._selected = set()
+        self.offset = 0
 
-  def __iter__(self):
-    self.i = self.offset
-    return self
+    def __iter__(self):
+        self.i = self.offset
+        return self
 
-  def __next__(self):
-    i = self.i
-    if i >= len(self.lines):
-      raise StopIteration
-    self.i += 1
+    def __next__(self):
+        i = self.i
+        if i >= len(self.lines):
+            raise StopIteration
+        self.i += 1
 
-    ret = self.lines[i]
-    if i in self._folded:
-      ret = PREFIX_FOLDED + ret
-      try:
-        if ret.startswith(FOLDED_DIFF):
-          pos = self.files.index(i)
-          self.i = self.files[pos + 1]
+        ret = self.lines[i]
+        if i in self._folded:
+            ret = PREFIX_FOLDED + ret
+            try:
+                if ret.startswith(FOLDED_DIFF):
+                    pos = self.files.index(i)
+                    self.i = self.files[pos + 1]
+                else:
+                    pos = self.chunks.index(i)
+                    self.i = self.chunks[pos + 1]
+            except IndexError:
+                self.i = len(self.lines)
+            except Exception as ex:
+                pass
+        return ret, i, (i in self._selected)
+
+    # ~ def get_fold_range(self, i):
+        # ~ if not self.is_foldable(i):
+            # ~ i = self.find_foldable_before(i)
+        # ~ l = self.lines[i]
+        # ~ try:
+            # ~ if l.startswith(PREFIX_DIFF):
+                # ~ pos = self.files.index(i)
+                # ~ return i, self.files[pos + 1]
+            # ~ elif l.startswith(PREFIX_CHUNK):
+                # ~ pos = self.chunks.index(i)
+                # ~ return i, self.chunks[pos + 1]
+            # ~ except IndexError:
+                # ~ return i, len(self.lines)
+
+    def is_foldable(self, i):
+            l = self.lines[i]
+            return l.startswith(PREFIX_DIFF) or l.startswith(PREFIX_CHUNK)
+
+    def find_foldable_before(self, i):
+        while i > 0 and not self.is_foldable(i):
+            i -= 1
+        return i
+
+    def find_foldable_after(self, i):
+        m = len(self.lines)
+        while i < m and not self.is_foldable(i):
+            i += 1
+        return i
+
+    def toggle_fold(self, orig_i):
+        if self.is_foldable(orig_i):
+            i = orig_i
         else:
-          pos = self.chunks.index(i)
-          self.i = self.chunks[pos + 1]
-      except IndexError:
-        self.i = len(self.lines)
-      except Exception as ex:
-        pass
-    return ret, i, (i in self._selected)
+            i = self.find_foldable_before(orig_i)
+        if i in self._folded:
+            self._folded.remove(i)
+        else:
+            self._folded.add(i)
+        return orig_i - i
 
-  def is_foldable(self, i):
-      l = self.lines[i]
-      return l.startswith(PREFIX_DIFF) or l.startswith(PREFIX_CHUNK)
+    def toggle_select(self, i):
+        if self.is_foldable(i):
+            if i in self._selected:
+                self._selected.remove(i)
+            else:
+                self._selected.add(i)
+        elif i in self._selected:
+            self._selected.remove(i)
+        else:
+            self._selected.add(i)
 
-  def find_foldable_before(self, i):
-    while i > 0 and not self.is_foldable(i):
-      i -= 1
-    return i
+    @classmethod
+    def from_file(cls, path):
+        lines = []
+        files = []
+        chunks = []
+        with open(path) as f:
+            for i, l in enumerate(f):
+                lines.append(l[:-1])
+                if l.startswith(PREFIX_DIFF):
+                    files.append(i)
+                elif l.startswith(PREFIX_CHUNK):
+                    chunks.append(i)
 
-  def find_foldable_after(self, i):
-    m = len(self.lines)
-    while i < m and not self.is_foldable(i):
-      i += 1
-    return i
-
-  def toggle_fold(self, orig_i):
-    if self.is_foldable(orig_i):
-      i = orig_i
-    else:
-      i = self.find_foldable_before(orig_i)
-    if i in self._folded:
-      self._folded.remove(i)
-    else:
-      self._folded.add(i)
-    return orig_i - i
-
-  def toggle_select(self, i):
-    if i in self._selected:
-      self._selected.remove(i)
-    else:
-      self._selected.add(i)
-
-  @classmethod
-  def from_file(cls, path):
-    lines = []
-    files = []
-    chunks = []
-    with open(path) as f:
-      for i, l in enumerate(f):
-        lines.append(l[:-1])
-        if l.startswith(PREFIX_DIFF):
-          files.append(i)
-        elif l.startswith(PREFIX_CHUNK):
-          chunks.append(i)
-
-    return cls(lines, files, chunks)
+        return cls(lines, files, chunks)
 
 
 def get_style(l):
-  if l.startswith(PREFIX_DIFF) or l.startswith(FOLDED_DIFF):
-    return curses.A_BOLD # COLOR_WHITE
-  elif l.startswith('--- '):
-    return curses.A_BOLD | curses.color_pair(1) # COLOR_RED
-  elif l.startswith('+++ '):
-    return curses.A_BOLD | curses.color_pair(2) # COLOR_GREEN
-  elif l.startswith(PREFIX_CHUNK) or l.startswith(FOLDED_CHUNK):
-    return curses.A_BOLD | curses.color_pair(4) # COLOR_BLUE
-  elif l.startswith('-'):
-    return curses.color_pair(1) # COLOR_RED
-  elif l.startswith('+'):
-    return curses.color_pair(2) # COLOR_GREEN
-  return curses.A_DIM
+    if l.startswith(PREFIX_DIFF) or l.startswith(FOLDED_DIFF):
+        return curses.A_BOLD # COLOR_WHITE
+    elif l.startswith('--- '):
+        return curses.A_BOLD | curses.color_pair(1) # COLOR_RED
+    elif l.startswith('+++ '):
+        return curses.A_BOLD | curses.color_pair(2) # COLOR_GREEN
+    elif l.startswith(PREFIX_CHUNK) or l.startswith(FOLDED_CHUNK):
+        return curses.A_BOLD | curses.color_pair(4) # COLOR_BLUE
+    elif l.startswith('-'):
+        return curses.color_pair(1) # COLOR_RED
+    elif l.startswith('+'):
+        return curses.color_pair(2) # COLOR_GREEN
+    return curses.A_DIM
 
 
 def main(scr):
-  curses.noecho()
-  # make curses give nice values (such as curses.KEY_LEFT)
-  scr.keypad(True)
-  scr.clear()
+    curses.noecho()
+    # make curses give nice values (such as curses.KEY_LEFT)
+    scr.keypad(True)
+    scr.clear()
 
-  curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
-  curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
-  curses.init_pair(3, curses.COLOR_YELLOW, curses.COLOR_BLACK)
-  curses.init_pair(4, curses.COLOR_BLUE, curses.COLOR_BLACK)
-  curses.init_pair(5, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
-  curses.init_pair(6, curses.COLOR_CYAN, curses.COLOR_BLACK)
-  curses.init_pair(7, curses.COLOR_WHITE, curses.COLOR_BLACK)
+    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+    curses.init_pair(4, curses.COLOR_BLUE, curses.COLOR_BLACK)
+    curses.init_pair(5, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
+    curses.init_pair(6, curses.COLOR_CYAN, curses.COLOR_BLACK)
+    curses.init_pair(7, curses.COLOR_WHITE, curses.COLOR_BLACK)
 
-  diff_path = sys.argv[1]
-  d = FoldingDiff.from_file(diff_path)
+    diff_path = sys.argv[1]
+    d = FoldingDiff.from_file(diff_path)
 
-  cur_y = 0
-  while True:
-    # First, clear the screen
-    scr.erase()
-    my, mx = scr.getmaxyx()
-    mx -= 2
-    my -= 2
+    cur_y = 0
+    while True:
+        # First, clear the screen
+        scr.erase()
+        my, mx = scr.getmaxyx()
+        mx -= 2
+        my -= 2
 
-    # print on screen here
-    scr.border(0)
+        # print on screen here
+        scr.border(0)
 
-    row_index = []
-    for i, (line, j, selected) in enumerate(d):
-      if i >= my:
-        break
-      style = get_style(line)
-      if len(line) > mx:
-        line = line[:mx] + PREFIX_FOLDED
-      row_index.append(j)
-      scr.addstr(i + 1, 1, line + f' <{j}', style)
-      if selected:
-        scr.addstr(i + 1, 0, "=")
+        row_index = []
+        for i, (line, j, selected) in enumerate(d):
+            if i >= my:
+                break
+            style = get_style(line)
+            if len(line) > mx:
+                line = line[:mx] + PREFIX_FOLDED
+            row_index.append(j)
+            scr.addstr(i + 1, 1, line + f' <{j}', style)
+            if selected:
+                scr.addstr(i + 1, 0, "=")
 
-    scr.addstr(0, 4, f"[{diff_path[-20:]}] /{mx} {cur_y}/{my} {row_index[cur_y]}")
+        scr.addstr(0, 4, f"[{diff_path[-20:]}] /{mx} {cur_y}/{my} {row_index[cur_y]}")
 
-    scr.move(cur_y + 1, 1)
-
-    # Draw the screen
-    scr.refresh()
-
-    # Wait for a keystroke
-    c = scr.getch()
-    if c == ord('q'):
-      break  # Exit the while loop
-    elif c == ord('c'):
-      break  # execute cut
-    elif c == ord('\t'):
-      cur_y -= d.toggle_fold(row_index[cur_y])
-    elif c == ord(' '):
-      d.toggle_select(row_index[cur_y])
-    elif c == curses.KEY_UP:
-      if cur_y > 0:
-        cur_y -= 1
         scr.move(cur_y + 1, 1)
-      elif d.offset > 0:
-        d.offset -= 1
-    elif c == curses.KEY_DOWN:
-      if cur_y < my - 5:
-        cur_y += 1
-        scr.move(cur_y + 1, 1)
-      else:
-        d.offset += 1
-    elif c == curses.KEY_HOME:
-        d.offset = 0
-        cur_y = 0
-    elif c == curses.KEY_END:
-      pass
-    elif c == curses.KEY_PPAGE:
-      if d.offset >= my:
-        d.offset -= my
-      else:
-        d.offset = 0
-        cur_y = 0
-    elif c == curses.KEY_NPAGE:
-      d.offset += my
-    elif c == curses.KEY_LEFT:
-      y = d.find_foldable_before(row_index[cur_y])
-    elif c == curses.KEY_RIGHT:
-      y = d.find_foldable_after(row_index[cur_y])
+
+        # Draw the screen
+        scr.refresh()
+
+        # Wait for a keystroke
+        c = scr.getch()
+        if c == ord('q'):
+            break    # Exit the while loop
+        elif c == ord('c'):
+            break    # execute cut
+        elif c == ord('\t'):
+            cur_y -= d.toggle_fold(row_index[cur_y])
+        elif c == ord(' '):
+            d.toggle_select(row_index[cur_y])
+        elif c == curses.KEY_UP:
+            if cur_y > 0:
+                cur_y -= 1
+                scr.move(cur_y + 1, 1)
+            elif d.offset > 0:
+                d.offset -= 1
+        elif c == curses.KEY_DOWN:
+            if cur_y < my - 5:
+                cur_y += 1
+                scr.move(cur_y + 1, 1)
+            else:
+                d.offset += 1
+        elif c == curses.KEY_HOME:
+                d.offset = 0
+                cur_y = 0
+        elif c == curses.KEY_END:
+            pass
+        elif c == curses.KEY_PPAGE:
+            if d.offset >= my:
+                d.offset -= my
+            else:
+                d.offset = 0
+                cur_y = 0
+        elif c == curses.KEY_NPAGE:
+            d.offset += my
+        elif c == curses.KEY_LEFT:
+            y = d.find_foldable_before(row_index[cur_y])
+        elif c == curses.KEY_RIGHT:
+            y = d.find_foldable_after(row_index[cur_y])
 
 curses.wrapper(main)
